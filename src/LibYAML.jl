7c847dfab4992430cd30b7516e267a566332fb00
MichaelHatherly <michaelhatherly@gmail.com>
2024-04-19 10:48:27 +0100

    Pull yaml-test-suite in tests and load all test files

--- src/LibYAML.jl
+++ src/LibYAML.jl
@@ -1,6 +1,6 @@
 module LibYAML
 
-module API
+module C_API
 
 using LibYAML_jll
 
@@ -13,6 +13,72 @@ end
 
 Get the version of the LibYAML library used by this package.
 """
-version() = VersionNumber(unsafe_string(API.yaml_get_version_string()))
+version() = VersionNumber(unsafe_string(C_API.yaml_get_version_string()))
+
+"""
+    load(text::AbstractString) -> YAML
+
+Load a YAML string and return the parsed data. The data is returned as a nested
+structure of `Vector`s, `Dict`s, and scalar values.
+"""
+load(text::AbstractString) = _parse_yaml_str(text)
+
+"""
+    load_file(filename::AbstractString) -> YAML
+
+Load a YAML file and return the parsed data. The data is returned as a nested
+structure of `Vector`s, `Dict`s, and scalar values.
+"""
+load_file(filename::AbstractString) = load(read(filename, String))
+
+function _parse_yaml_str(text::AbstractString)
+    parser = Ref{C_API.yaml_parser_s}()
+    event = Ref{C_API.yaml_event_s}()
+    C_API.yaml_parser_initialize(parser)
+    text = string(text)
+    len = ncodeunits(text)
+    C_API.yaml_parser_set_input_string(parser, text, len)
+    try
+        return _handle_events(parser, event, [])
+    finally
+        C_API.yaml_event_delete(event)
+        C_API.yaml_parser_delete(parser)
+    end
+end
+
+function _handle_events(parser, event, acc)
+    while true
+        C_API.yaml_parser_parse(parser, event) == 0 &&
+            throw(YAMLParserError("Failed to parse YAML"))
+
+        type = event[].type
+        if type == C_API.YAML_NO_EVENT
+        elseif type == C_API.YAML_STREAM_START_EVENT
+        elseif type == C_API.YAML_STREAM_END_EVENT
+            return acc
+        elseif type == C_API.YAML_DOCUMENT_START_EVENT
+        elseif type == C_API.YAML_DOCUMENT_END_EVENT
+        elseif type == C_API.YAML_ALIAS_EVENT
+        elseif type == C_API.YAML_SCALAR_EVENT
+            scalar = event[].data.scalar
+            push!(acc, unsafe_string(scalar.value, scalar.length))
+        elseif type == C_API.YAML_MAPPING_START_EVENT
+            push!(acc, _handle_events(parser, event, []))
+        elseif type == C_API.YAML_MAPPING_END_EVENT
+            count = length(acc)
+            iseven(count) || error("unreachable reached, expected even number in mapping.")
+            mapping = Dict{Any,Any}()
+            for (k, v) in Iterators.partition(acc, 2)
+                haskey(mapping, k) && error("Map keys must be unique: $k")
+                mapping[k] = v
+            end
+            return mapping
+        elseif type == C_API.YAML_SEQUENCE_START_EVENT
+            push!(acc, _handle_events(parser, event, []))
+        elseif type == C_API.YAML_SEQUENCE_END_EVENT
+            return acc
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using LibYAML
 using Test
 
+yaml_test_suite = joinpath(@__DIR__, "yaml-test-suite")
+let yaml_test_suite_commit = "45db50aecf9b1520f8258938c88f396e96f30831"
+    if isdir(yaml_test_suite)
+        cd(yaml_test_suite) do
+            run(`git fetch`)
+        end
+    else
+        run(`git clone https://github.com/yaml/yaml-test-suite`)
+    end
+    cd(yaml_test_suite) do
+        run(`git checkout $yaml_test_suite_commit`)
+    end
+end
+
 @testset "LibYAML" begin
     @test LibYAML.version() == v"0.2.5"
+
+    @testset "yaml-test-suite" begin
+        yaml_test_suite_src = joinpath(yaml_test_suite, "src")
+        for (root, dirs, files) in walkdir(yaml_test_suite_src), file in files
+            path = joinpath(root, file)
+            if endswith(path, ".yaml")
+                @testset "$path" begin
+                    LibYAML.load_file(path)
+                end
+            end
+        end
+    end
 end
